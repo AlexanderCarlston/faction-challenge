b10be29a89c1c5d6a2eeb744bfc021c8316569a9
alekscarlston@yahoo.com <alekscarlston@yahoo.com>
2018-12-26 14:22:36 -0700

    connected template to ts

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -7,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
 
-  
+  token: string = ""
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  submit(){
+    console.log("worked")
+  }
 }
